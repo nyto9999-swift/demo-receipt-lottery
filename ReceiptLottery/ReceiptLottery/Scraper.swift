b0161a8a56e76443dc64e28ce2e77937a5d5c651
ninjachen <nyto9999@gmail.com>
2022-04-01 07:08:42 +0800

    fixed incorrect output number, done

--- ReceiptLottery/ReceiptLottery/Scraper.swift
+++ ReceiptLottery/ReceiptLottery/Scraper.swift
@@ -1,16 +1,16 @@
 import UIKit
 import Kanna
 
-class Scraper {
+final class Scraper {
     static let shared = Scraper()
     var awards        = [Award]()
     
-    func getRecieptNumbers(URLStr: String, completion: @escaping (Result<(awards: [Award],footerText: String, titleText: String), Error>) -> Void) {
+    func getRecieptNumbers(URLStr: String, completion: @escaping (Result<(awards: [Award],footerText: String?, titleText: String?), Error>) -> Void) {
         let session = URLSession.shared
         let url = URL(string: URLStr)!
-        let task = session.dataTask(with: url) { data, response, error in
+        let task = session.dataTask(with: url) { [weak self] data, response, error in
             
-            guard let loadedData = data, error == nil else {
+            guard let self = self, let loadedData = data, error == nil else {
                 completion(.failure(error!))
                 return
             }
@@ -19,12 +19,10 @@ class Scraper {
                         
             do {
                 let parsedHTML = try Kanna.HTML(html: contents, encoding: String.Encoding.utf8)
-            
-                let totalAwards = parsedHTML.xpath("//div[@class ='etw-web']//tbody/tr/td[@headers = 'th01']").count
-                
+                let totalAwardsCount = parsedHTML.xpath("//div[@class ='etw-web']//tbody/tr/td[@headers = 'th01']").count
                 let firstAwardNumberOfArray = self.getFirstAward(parsedHTML)
                 
-                for ix in 1...totalAwards {
+                for ix in 1...totalAwardsCount {
                     
                     let title = parsedHTML.xpath("//div[@class ='etw-web']//tr[\(ix)]/td[@headers = 'th01']").first?.text
                     let number = parsedHTML.xpath("//div[@class = 'etw-web']//tr[\(ix)]//span").first?.text
@@ -34,7 +32,7 @@ class Scraper {
                             
                         //特別獎, 特獎
                         case 1...2:
-                            self.awards.append(Award(title: title, numbers: [Int(number!)], description: description))
+                            self.awards.append(Award(title: title, numbers: [Int(number ?? "資料錯誤")], description: description))
             
                         //case 3...8: 頭獎
                             
@@ -45,18 +43,14 @@ class Scraper {
                             completion(.failure(ScrapeError.defaultBreak))
                     }
                 }
-                let title = parsedHTML.xpath("//div[@class ='etw-web']//ul/li[1]")
-                guard let headerText = title.first?.text! else {
-                    completion(.failure(ScrapeError.title))
-                    return
-                }
                 
-                let footer = parsedHTML.xpath("//div[@class ='etw-web']//tfoot//td")
-                guard let footerText = footer.first?.text! else {
-                    completion(.failure(ScrapeError.footer))
-                    return
-                }
+                //標題
+                let headerText = parsedHTML.xpath("//div[@class ='etw-web']//ul/li[1]").first?.text
                 
+                //footer
+                let footerText = parsedHTML.xpath("//div[@class ='etw-web']//tfoot//td").first?.text
+                
+                //傳到view
                 completion(.success((self.awards, footerText, headerText)))
             }
             catch {
@@ -66,7 +60,7 @@ class Scraper {
         task.resume()
     }
     
-    //頭獎
+    //頭獎 拼接字串
     func getFirstAward(_ parsedHTML: HTMLDocument) -> [Int] {
         let rhsTexts = parsedHTML.xpath("//div[@class ='etw-mobile']//tr[3]/td[2]/p[@class = 'etw-tbiggest mb-md-4']/span[@class = 'font-weight-bold etw-color-red']")
         let lhsTexts = parsedHTML.xpath("//div[@class ='etw-mobile']//tr[3]/td[2]/p[@class = 'etw-tbiggest mb-md-4']/span[@class = 'font-weight-bold']")
--- ReceiptLottery/ReceiptLottery/ViewController.swift
+++ ReceiptLottery/ReceiptLottery/ViewController.swift
@@ -4,8 +4,6 @@ import Kanna
 class ViewController: UIViewController {
 
     var awards = [Award]()
-    var titleText:String = ""
-    var footerText:String = ""
     let customFont:UIFont = UIFont.init(name: "AppleSDGothicNeo-Bold", size: 40.0)!
     
     lazy var textField:UITextField = {
@@ -13,10 +11,12 @@ class ViewController: UIViewController {
                                                   y: view.frame.size.height / 5,
                                                   width: 280,
                                                   height: 80))
-        textField.backgroundColor          = .lightGray
+        textField.backgroundColor          = .lightGray.withAlphaComponent(0.4)
         textField.borderStyle              = .roundedRect
         textField.clearButtonMode          = .whileEditing
         textField.font                     = customFont
+        textField.textColor                = .lightGray
+        
         return textField
     }()
     
@@ -45,7 +45,7 @@ class ViewController: UIViewController {
     }()
     
     lazy var titleLabel    = label()
-    lazy var subTitleLabel = label()
+    lazy var awardLabel    = label()
     lazy var numberLabel   = label()
     lazy var descLabel     = label()
     
@@ -54,18 +54,19 @@ class ViewController: UIViewController {
         
         let URLStr = "https://invoice.etax.nat.gov.tw/index.html"
         Scraper.shared.getRecieptNumbers(URLStr: URLStr,completion: { [weak self] result in
+            guard let self = self else { return }
+
             switch result {
                 case .success(let response):
-                    self?.awards = response.awards
-                    self?.footerText = response.footerText
-                    self?.titleText = response.titleText
-                    
+                    DispatchQueue.main.async {
+                        self.awards = response.awards
+                        self.footerLabel.text = response.footerText
+                        self.titleLabel.text = response.titleText
+                    }
                 case .failure(let error):
                     print(error)
             }
         })
-    }
-    override func viewDidLayoutSubviews() {
         setupViews()
         setupConstraints()
     }
@@ -75,13 +76,9 @@ class ViewController: UIViewController {
         numberLabel.textColor    = .systemOrange
         titleLabel.textAlignment = .center
         
-        //wait for asyn function
-        footerLabel.text = footerText
-        titleLabel.text  = titleText
-        
         view.addSubview(infoLabel)
         view.addSubview(titleLabel)
-        view.addSubview(subTitleLabel)
+        view.addSubview(awardLabel)
         view.addSubview(numberLabel)
         view.addSubview(descLabel)
         view.addSubview(textField)
@@ -92,17 +89,17 @@ class ViewController: UIViewController {
     func setupConstraints() {
 
         titleLabel.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 10).isActive = true
-        subTitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10).isActive = true
-        numberLabel.topAnchor.constraint(equalTo: subTitleLabel.bottomAnchor, constant: 10).isActive = true
+        awardLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10).isActive = true
+        numberLabel.topAnchor.constraint(equalTo: awardLabel.bottomAnchor, constant: 10).isActive = true
         descLabel.topAnchor.constraint(equalTo: numberLabel.bottomAnchor, constant: 10).isActive = true
         
         titleLabel.leadingAnchor.constraint(equalTo: textField.leadingAnchor).isActive = true
-        subTitleLabel.leadingAnchor.constraint(equalTo: textField.leadingAnchor).isActive = true
+        awardLabel.leadingAnchor.constraint(equalTo: textField.leadingAnchor).isActive = true
         numberLabel.leadingAnchor.constraint(equalTo: textField.leadingAnchor).isActive = true
         descLabel.leadingAnchor.constraint(equalTo: textField.leadingAnchor).isActive = true
         
         titleLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        subTitleLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        awardLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
         numberLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
         descLabel.heightAnchor.constraint(equalToConstant: 80).isActive = true
     }
@@ -111,78 +108,97 @@ class ViewController: UIViewController {
 
 extension ViewController: UITextFieldDelegate {
     
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        
-        guard let textFieldText = textField.text, textFieldText.count == 8 else {
-            self.infoLabel.text = "請輸入八個數字"
-            self.numberLabel.text = ""
-            self.descLabel.text = ""
-            
-            return
-        }
-        
-        var userInput = Int(textFieldText)
-        var isNotBingo = true
-        let firtPrizeNumbers = awards.filter { $0.title == "頭獎"}.first!.numbers
-        
-        for ix in 0...2 {
-            
-            for number in awards[ix].numbers {
-                
-                //號碼全對，特別獎，特獎，頭獎
-                if userInput == number {
-                    
-                    DispatchQueue.main.async {
-                        self.subTitleLabel.text = self.awards[ix].title
-                        self.numberLabel.text = "\(number!)"
-                        self.descLabel.text = self.awards[ix].description
-                        self.textField.textColor = .systemGreen
-                    }
-                    isNotBingo.toggle()
-                }
-            }
-        }
-        
-        //查看是否中 二獎，三獎，肆獎，五獎，六獎
-        if isNotBingo {
-            var numbersDic = [Int:Int]()
-            
-            for (index, number) in firtPrizeNumbers.enumerated() {
-                var correctNumbers = 0
-                var number = number
-                
-                while (userInput ?? 0) % 10 == (number ?? 0) % 10 {
-                        
-                    userInput! /= 10
-                    number! /= 10
-                    correctNumbers += 1
-                
-                }
-                numbersDic[index] = correctNumbers
-            }
-            let biggestNumber = numbersDic.values.max()!
-            let bingoIndex = numbersDic.first(where: { $0.value == biggestNumber})?.key
-            guard let bingoIndex = bingoIndex else { return }
-
-            //中獎
-            if biggestNumber >= 3 {
-                DispatchQueue.main.async {
-                    self.subTitleLabel.text = self.awards[10 - biggestNumber].title
-                    self.numberLabel.text = "\(String(describing: self.awards[10 - biggestNumber].numbers[bingoIndex]!))"
-                    self.descLabel.text = self.awards[10 - biggestNumber].description
-                    self.infoLabel.text = "恭喜中獎"
-                }
-            }
-            //沒有中獎
-            else {
-                self.infoLabel.text = "沒中獎"
-            }
-        }
-    }
-    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         view.endEditing(true)
     }
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        
+        if textField.text?.count == 8 {
+            self.textField.textColor = .darkGray
+            
+            guard let textFieldText = textField.text, textFieldText.count == 8 else {
+                self.textField.placeholder = "請輸入八個數字"
+                self.numberLabel.text = ""
+                self.descLabel.text = ""
+                return
+            }
+            
+            let userInput = Int(textFieldText)
+            var isNotAllCorrect = true
+            
+            for ix in 0...2 {
+                
+                for number in awards[ix].numbers {
+                    
+                    //號碼全對，特別獎，特獎，頭獎
+                    if userInput == number {
+                        
+                        DispatchQueue.main.async {
+                            self.awardLabel.text = self.awards[ix].title
+                            self.numberLabel.text = "\(number ?? 0)"
+                            self.descLabel.text = self.awards[ix].description
+                            self.infoLabel.text = "恭喜中獎"
+                            self.textField.textColor = .systemOrange
+                        }
+                        isNotAllCorrect = false
+                    }
+                }
+            }
+            
+            if isNotAllCorrect {
+                let firstAwardNumbers = awards.filter { $0.title == "頭獎"}.first!.numbers
+                var firstAwardDictionary = [Int:Int]()
+                
+                //頭獎有三組號碼，查看是否中 二獎，三獎，肆獎，五獎，六獎
+                for (index, receiptNumber) in firstAwardNumbers.enumerated() {
+                    var correctNumbers = 0
+                    var cReceiptNumber = receiptNumber
+                    var cUserInput = userInput
+                    
+                    while (cUserInput ?? 0) % 10 == (cReceiptNumber ?? 0) % 10 {
+                        
+                        cUserInput! /= 10
+                        cReceiptNumber! /= 10
+                        correctNumbers += 1
+                        
+                    }
+                    firstAwardDictionary[index] = correctNumbers
+                }
+                
+                //取出最大獎
+                let biggestNumber = firstAwardDictionary.values.max()!
+                let bingoIndex = firstAwardDictionary.first(where: { $0.value == biggestNumber})?.key
+                guard let bingoIndex = bingoIndex else { return }
+                
+                //中獎
+                if biggestNumber >= 3 {
+                    let award = self.awards.first(where: {$0.description!.contains("中獎號碼末\(biggestNumber)")})
+                    guard let award = award else {
+                        self.titleLabel.text = "系統錯誤，有bug"
+                        return
+                    }
+                    DispatchQueue.main.async {
+                        self.awardLabel.text = award.title
+                        self.numberLabel.text = "\(String(describing: award.numbers[bingoIndex]!))"
+                        self.descLabel.text = award.description
+                        self.infoLabel.text = "恭喜中獎"
+                    }
+                }
+                //沒有中獎
+                else {
+                    self.infoLabel.text = "沒中獎"
+                    self.awardLabel.text = ""
+                    self.numberLabel.text = ""
+                    self.descLabel.text = ""
+                }
+            }
+        }
+        else {
+            self.textField.textColor = .lightGray
+        }
+    }
+    
 }
 
 
